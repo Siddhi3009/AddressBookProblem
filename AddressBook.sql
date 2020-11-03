039858c372324954fae210a94b8cd342df0c7171
Administrator <siddhiseth3009@gmail.com>
2020-11-04 02:36:34 +0530

    [Siddhi]Add. ContactInfo amd ContactType tables added

--- AddressBook.sql
+++ AddressBook.sql
@@ -67,3 +67,43 @@ group by Contact_Type
 --Adding contact bill to family also
 insert into Address_Book values
 ('Bill','Jones','Street 4','Mumbai','Maharashtra','452856','9856985696','billjones@gmail.com','Family','Home');
+--ContactType table added
+create table Contact_Type
+(
+FirstName varchar(25) not null,
+Contact_Type varchar(20) not null
+);
+--Add enteries to contact_type
+insert into Contact_Type values
+('Bill','Friends'),
+('Leena','Family'),
+('Terrisa','Friends'),
+('Priyanka','Friends'),
+('Karishma','Family'),
+('Rakhi','Professional'),
+('Bill', 'Family');
+--View Contact_type
+select * from Contact_Type
+--Create table Contact_Info
+create table Contact_Info
+(
+FirstName varchar(25) not null,
+LastName varchar(25) not null,
+Address varchar(60) not null,
+City varchar(15) not null,
+State varchar(20) not null,
+Zipcode varchar(6) not null,
+PhoneNumber varchar(12) not null,
+Email varchar(20) not null
+);
+--Insert into Contact_Info
+insert into Contact_Info values
+('Bill','Jones','Street 4','Mumbai','Maharashtra','452856','9856985696','billjones@gmail.com'),
+('Leena','Thomas','New Market','Bhopal','Madhya Pradesh','852147','7458632156','leena@gmail.com'),
+('Terrisa','Gates','Sector 10','Bhopal','Madhya Pradesh','875489','8425693856','gates@gmail.com'),
+('Priyanka','Chopra','Malviya Nagar','Ajmer','Rajasthan','547856','9589657485','priyanka@gmail.com'),
+('Karishma','Khanna','Gopal Vihar','Bhopal','Madhya Pradesh','658927','9424787845','karishma@gmail.com'),
+('Rakhi','Saraf','Manik Nagar','Ajmer','Rajasthan','125463','8596785425','rakhi@gmail.com');
+--Join contact_info and contact_type
+select * from Contact_Info contact inner join Contact_Type type
+on (contact.FirstName = type.FirstName)
