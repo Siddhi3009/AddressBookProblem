98ab61e5ea15543b78cb19d89434b53aef087f53
Administrator <siddhiseth3009@gmail.com>
2020-11-09 19:35:19 +0530

    [Siddhi]Add. GetAllContacts method added

--- AddressBook.sql
+++ AddressBook.sql
@@ -70,45 +70,57 @@ insert into Address_Book values
 --Create table Contact_Info
 create table Contact_Info
 (
-FirstName varchar(25) not null primary key,
+ContactId int identity(1,1) not null primary key,
+FirstName varchar(25) not null,
 LastName varchar(25) not null,
-Address varchar(60) not null,
-City varchar(15) not null,
-State varchar(20) not null,
-Zipcode varchar(6) not null,
 PhoneNumber varchar(12) not null,
 Email varchar(20) not null
 );
 --Insert into Contact_Info
 insert into Contact_Info values
-('Bill','Jones','Street 4','Mumbai','Maharashtra','452856','9856985696','billjones@gmail.com'),
-('Leena','Thomas','New Market','Bhopal','Madhya Pradesh','852147','7458632156','leena@gmail.com'),
-('Terrisa','Gates','Sector 10','Bhopal','Madhya Pradesh','875489','8425693856','gates@gmail.com'),
-('Priyanka','Chopra','Malviya Nagar','Ajmer','Rajasthan','547856','9589657485','priyanka@gmail.com'),
-('Karishma','Khanna','Gopal Vihar','Bhopal','Madhya Pradesh','658927','9424787845','karishma@gmail.com'),
-('Rakhi','Saraf','Manik Nagar','Ajmer','Rajasthan','125463','8596785425','rakhi@gmail.com');
+('Bill','Jones','9856985696','billjones@gmail.com'),
+('Leena','Thomas','7458632156','leena@gmail.com'),
+('Terrisa','Gates','8425693856','gates@gmail.com'),
+('Priyanka','Chopra','9589657485','priyanka@gmail.com'),
+('Karishma','Khanna','9424787845','karishma@gmail.com'),
+('Rakhi','Saraf','8596785425','rakhi@gmail.com');
 
 --ContactType table added
 create table Contact_Type
 (
-FirstName varchar(25) not null foreign key references Contact_Info(FirstName),
+ContactId int foreign key references Contact_Info(ContactId) on delete cascade,
 Contact_Type varchar(20) not null
 );
 --Add enteries to contact_type
 insert into Contact_Type values
-('Bill','Friends'),
-('Leena','Family'),
-('Terrisa','Friends'),
-('Priyanka','Friends'),
-('Karishma','Family'),
-('Rakhi','Professional'),
-('Bill', 'Family');
+(1,'Friends'),
+(2,'Family'),
+(3,'Friends'),
+(4,'Friends'),
+(5,'Family'),
+(6,'Professional');
+--create Address table
+create table Address
+(
+ContactId int foreign key references Contact_Info(ContactId) on delete cascade,
+Address varchar(60) not null,
+City varchar(15) not null,
+State varchar(20) not null,
+Zipcode varchar(6) not null
+);
+--Insert into Address table
+insert into Address values
+(1,'Street 4','Mumbai','Maharashtra','452856'),
+(2,'New Market','Bhopal','Madhya Pradesh','852147'),
+(3,'Malviya Nagar','Ajmer','Rajasthan','547856'),
+(4,'Gopal Vihar','Bhopal','Madhya Pradesh','658927'),
+(5,'Manik Nagar','Ajmer','Rajasthan','125463');
 --View Contact_type
 select * from Contact_Type
---Join contact_info and contact_type
-select * from Contact_Info contact inner join Contact_Type type
-on (contact.FirstName = type.FirstName)
+--Join contact_info and contact_type and Address
+select * from (Contact_Info contact inner join Contact_Type type
+on (contact.ContactId = type.ContactId)) inner join Address address on address.ContactId = contact.ContactId
 --Count contact by type
 select type.Contact_Type, COUNT(contact.FirstName) from Contact_Info contact inner join Contact_Type type
-on (contact.FirstName = type.FirstName)
+on (contact.ContactId = type.ContactId)
 Group by type.Contact_Type;
